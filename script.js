0398ed34aa802fdac29d3ca3a615d9cb1e49ee2d
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-03 08:02:51 -0300

    Resolução do quinto e sexto requisito

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function createList() {
   document.body.appendChild(list);
 }
 createList();
+
+function createButton() {
+  let button = document.createElement('button');
+  let divTask = document.querySelector('.tasksEntries');
+  button.id = 'criar-tarefa';
+  button.innerText = 'Criar tarefa'
+  divTask.appendChild(button);
+}
+createButton();
+
+let btnAddTask = document.querySelector('#criar-tarefa');
+btnAddTask.addEventListener('click', function() {
+  let input = document.querySelector('input');
+  let list = document.querySelector('ol');
+  let item = document.createElement('li');
+  item.innerText = input.value;
+  list.appendChild(item);
+  input.value = '';
+})
