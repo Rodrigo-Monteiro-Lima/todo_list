c0d73e13b003389039426116eabf6ef32e0b84f7
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-03 08:47:41 -0300

    Resolução do sétimo requisito

--- script.js
+++ script.js
@@ -15,11 +15,23 @@ function createButton() {
 createButton();
 
 let btnAddTask = document.querySelector('#criar-tarefa');
+let list = document.querySelector('ol');
 btnAddTask.addEventListener('click', function() {
   let input = document.querySelector('input');
-  let list = document.querySelector('ol');
   let item = document.createElement('li');
   item.innerText = input.value;
   list.appendChild(item);
   input.value = '';
+})
+
+//let li = document.querySelectorAll('li');
+list.addEventListener('click', (event) => {
+  //console.log(li.length);
+  /*for (let index = 0; index < li.length; index += 1) {
+    li[index].classList.remove('selected') 
+  };*/
+  //list.childNodes.classList.remove('selected')
+  event.target.classList.add('selected');
+  //li.classList.remove('selected') 
+  
 })
